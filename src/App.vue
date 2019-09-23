e40425ba48a003994fe453a6498a4cd8a0d38633
rina0620 <fashiondrop@gmail.com>
2019-09-23 11:58:02 +0900

    yes noのif文を作ってもらった

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col md="6" offset-md="3">
+          <!-- QuenstionBox（データを渡したいコンポーネント）の中にv-bindで渡したいデータを定義する。 -->
+          <QuestionBox
+            v-if="questions != null"
+            v-bind:questions="questions[index].question"
+            v-bind:next="next"
+            v-bind:submit="submit"
+            v-bind:answer="answer"
+          /></b-col>
+      </b-row>
+    </b-container>
+    <!-- nullのまま表示するなって指示 -->
+    <p
+    v-if="questions != null"
+    >
+    <!-- {{ index }}<br> デバッグ用 -->
+    <!-- {{ correctAnswer }}
+    {{ questions }} -->
+    </p>
   </div>
 </template>
 
@@ -14,6 +35,37 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return { //dataファンクションに返り値を入れる(jsonオブジェクトにする必要がある)
+      questions: null, //変数の初期値を入れなきゃいけなくてnullは型を気にしなくていい形式（0で初期化するのと同じ。nullはintでも文字列でもない）
+      index: 0,
+      answer: ''
+    }
+  },
+  mounted() {
+    fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean', {
+      method: 'get' //getでapiを叩いてる
+    })
+      .then((response) => {
+        return response.json() //apiのレスポンスをresponseと言う名前で受けとってjson形式に変換してる
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results //上でリターンしたjsonオブジェクトのresultsキーのバリューをデータメソッドで定義したquestionsに代入してる
+      })
+  },
+  methods: {
+    next() { //index（配列の何番目を表示したい）のデータは App.vueが持ってるからここで書く。
+      this.index++ //++で順番を増やすnext関数を作ってる
+    },
+    submit(selectedAnswer) {
+      this.answer = this.correctAnswer === selectedAnswer ? '正解！' : '不正解！'
+    }
+  },
+  computed: {
+    correctAnswer() {
+      return this.questions[this.index].correct_answer
+    }
   }
 }
 </script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <h1>Question Box</h1>
+    <b-jumbotron class="my-quiz-container">
+      <template v-slot:lead>
+        {{ questions }}
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        {{ answer }}
+        <!-- 正解！ 出し入れしたい -->
+      </p>
+
+      <b-button variant="primary" @click="submit('True')">Yes</b-button>
+      <b-button variant="danger" @click="submit('False')">No</b-button><br>
+    </b-jumbotron>
+    <b-button variant="info" @click="next()">Next</b-button> <!-- 出し入れしたい -->
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    questions: String,
+    next: Function,
+    submit: Function,
+    answer: String
+  }
+}
+</script>
